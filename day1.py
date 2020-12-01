aee8eb317d21f81b61b33e042cdc52ffd3481e72
meonwax <meonwax@meonwax.de>
2020-12-01 21:58:18 +0100

    Add solution for day 1

---
+++ day1.py
@@ -0,0 +1,20 @@
+f = open("day1.txt", "r")
+input = [int(line.strip()) for line in f.readlines()]
+f.close()
+
+print("\n# Part 1")
+for i, v1 in enumerate(input):
+    for v2 in input[i + 1:]:
+        if v1 + v2 == 2020:
+            print("{} + {} = {}".format(v1, v2, v1 + v2))
+            print("{} * {} = {}".format(v1, v2, v1 * v2))
+
+print("\n# Part 2")
+for i, v1 in enumerate(input):
+    for k, v2 in enumerate(input[i + 1:]):
+        if v1 + v2 > 2020:
+            continue
+        for v3 in input[k + 1:]:
+            if v1 + v2 + v3 == 2020:
+                print("{} + {} + {} = {}".format(v1, v2, v3, v1 + v2 + v3))
+                print("{} * {} * {} = {}".format(v1, v2, v3, v1 * v2 * v3))
