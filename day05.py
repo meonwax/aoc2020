4daa452c241f4a9bf77b6a033a44cbc268980880
meonwax <meonwax@meonwax.de>
2020-12-07 08:40:42 +0100

    Optimize solution for day 5

--- day05.py
+++ day05.py
@@ -1,43 +1,37 @@
+HALF_INDICATORS = {
+    'F': 0,
+    'B': 1,
+    'L': 0,
+    'R': 1
+}
+
 with open('day05.txt', 'r') as f:
     input = [line.strip() for line in f.readlines()]
 
 
-def get_row(boarding_pass):
-    lower = 0
-    upper = 128
-    for i in range(7):
-        diff = int((upper - lower) / 2)
-        if boarding_pass[i] == 'F':
-            upper -= diff
-        else:
-            lower += diff
-    return lower
-
-
-def get_column(boarding_pass):
-    lower = 0
-    upper = 8
-    for i in range(7, 10):
-        diff = int((upper - lower) / 2)
-        if boarding_pass[i] == 'L':
-            upper -= diff
-        else:
-            lower += diff
-    return lower
+def halve_interval(interval, half_indicator):
+    half = int(len(interval) / 2)
+    if half_indicator == 0:
+        return interval[:half]
+    if half_indicator == 1:
+        return interval[half:]
 
 
 print("\n# Part 1")
 seat_ids = []
 for boarding_pass in input:
-    row = get_row(boarding_pass)
-    col = get_column(boarding_pass)
-    seat_ids.append(row * 8 + col)
+    row_interval = [i for i in range(0, 128)]
+    cols_interval = [i for i in range(0, 8)]
+    for char in boarding_pass[:7]:
+        row_interval = halve_interval(row_interval, HALF_INDICATORS[char])
+    for char in boarding_pass[7:]:
+        cols_interval = halve_interval(cols_interval, HALF_INDICATORS[char])
+    seat_ids.append(row_interval[0] * 8 + cols_interval[0])
 print("Highest seat ID: {}".format(max(seat_ids)))
 
 print("\n# Part 2")
-seat_ids.sort()
-for i, seat_id in enumerate(seat_ids):
-    if seat_id + 2 == seat_ids[i + 1]:
+for seat_id in seat_ids:
+    if (seat_id + 2 in seat_ids) and not (seat_id + 1 in seat_ids):
         my_seat_id = seat_id + 1
         break
 print("My seat ID: {}".format(my_seat_id))
